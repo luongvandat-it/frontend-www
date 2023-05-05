1cc88cf5d7c19669e8bc2a3ef8204d27ed2854f0
luongvandat <luongvandat.it@gmail.com>
2023-05-05 09:42:39 +0700

    Update minus and plus in cart

--- js/cart.js
+++ js/cart.js
@@ -8,18 +8,19 @@ function loadCartPage() {
         content += "<td><img src='" + listBook[i].bookImage + "' width='100px' height='100px'></td>";
         content += "<td>" + listBook[i].bookName + "</td>";
         content += "<td>" + listBook[i].bookPrice + "</td>";
-        content += "<td><button hidden class='btn btn-primary' onclick='minusBook(\"" + listBook[i].bookName + "\")'>-</button>";
+        content += "<td><button class='btn-primary' style='border:none; border-radius: 4px;padding:10px' onclick='minusBook(\"" + listBook[i].bookName + "\"," + listBook[i].bookNumber + ");'>-</button>";
         content += "<input type='text' name='bookNumber' class='p-1' value='" + listBook[i].bookNumber + "' style='width: 50px; text-align: center; border:none' readonly>";
-        content += "<button hidden class='btn btn-primary' onclick='plusBook(\"" + listBook[i].bookName + "\")'>+</button></td>";
+        content += "<button class='btn-primary' style='border:none; border-radius: 4px;padding:10px' onclick='plusBook(\"" + listBook[i].bookName + "\"," + listBook[i].bookNumber + ");'>+</button></td>";
         var price = listBook[i].bookPrice.replace(" $", "");
         var totalPriceOfOne = parseFloat(price.trim()).toFixed(2) * listBook[i].bookNumber;
         content += "<td>" + totalPriceOfOne + " $</td>";
         content += "</tr>";
     }
     content += "<tr>";
-    content += "<td colspan='3'>Total Price Selected Book (Tax Included)</td>";
+    content += "<td colspan='3'>Total Price Selected Book (Tax 3% Included)</td>";
     content += "<td colspan='3'><b class='text-danger h3'>" + totalPrice + " $</b></td>";
     content += "</tr>";
+    localStorage.setItem("totalPriceInCart", totalPrice);
     $("tbody").append(content);
 }
 
@@ -39,6 +40,58 @@ function checkLogin() {
     return false;
 }
 
+function minusBook(bookName, bookNumber) {
+    var listBook = JSON.parse(localStorage.getItem("listBook"));
+    for (var i = 0; i < listBook.length; i++) {
+        if (listBook[i].bookName == bookName) {
+            if (bookNumber == 1) {
+                if (confirm("Do you want to remove this book from cart?")) {
+                    listBook.splice(i, 1);
+                    localStorage.setItem("listBook", JSON.stringify(listBook));
+                    $("tbody").empty();
+                    loadCartPage();
+                    return;
+                }
+            } else {
+                listBook[i].bookNumber = bookNumber - 1;
+            }
+        }
+        localStorage.setItem("listBook", JSON.stringify(listBook));
+
+        if ($("input[name='select'][value='" + bookName + "']").is(":checked")) {
+            var totalPrice = JSON.parse(localStorage.getItem("totalPriceInCart"));
+            totalPrice -= parseFloat(listBook[i].bookPrice.replace(" $", "").trim()).toFixed(2) * 1.03;
+            localStorage.setItem("totalPriceInCart", totalPrice);
+            $("tbody tr:last td:last").html("<b class='text-danger h3'>" + totalPrice.toFixed(2) + " $</b>");
+        }
+        $("tbody").empty();
+        loadCartPage();
+    }
+}
+
+function plusBook(bookName, bookNumber) {
+    var listBook = JSON.parse(localStorage.getItem("listBook"));
+    for (var i = 0; i < listBook.length; i++) {
+        if (listBook[i].bookName == bookName) {
+            if (bookNumber == 10) {
+                alert("You can only buy 10 books at a time!");
+            } else {
+                listBook[i].bookNumber = bookNumber + 1;
+            }
+        }
+        localStorage.setItem("listBook", JSON.stringify(listBook));
+
+        if ($("input[name='select'][value='" + bookName + "']").is(":checked")) {
+            var totalPrice = JSON.parse(localStorage.getItem("totalPriceInCart"));
+            totalPrice += parseFloat(listBook[i].bookPrice.replace(" $", "").trim()).toFixed(2) * 1.03;
+            localStorage.setItem("totalPriceInCart", totalPrice);
+            $("tbody tr:last td:last").html("<b class='text-danger h3'>" + totalPrice.toFixed(2) + " $</b>");
+        }
+        $("tbody").empty();
+        loadCartPage();
+    }
+}
+
 $(document).ready(function () {
     // Cart - load cart
     loadCartPage();
@@ -50,7 +103,7 @@ $(document).ready(function () {
         $("input[name='select']:checked").each(function () {
             var price = $(this).parent().parent().find("td:eq(3)").text().replace(" $", "");
             var bookNumber = $(this).parent().parent().find("input[name='bookNumber']").val();
-            totalPrice += parseFloat(price.trim()).toFixed(2) * bookNumber;
+            totalPrice += parseFloat(price.trim()).toFixed(2) * bookNumber * 1.03;
         });
         $("tbody tr:last td:last").html("<b class='text-danger h3'>" + totalPrice.toFixed(2) + " $</b>");
     }
@@ -62,7 +115,7 @@ $(document).ready(function () {
         $("input[name='select']:checked").each(function () {
             var price = $(this).parent().parent().find("td:eq(3)").text().replace(" $", "");
             var bookNumber = $(this).parent().parent().find("input[name='bookNumber']").val();
-            totalPrice += parseFloat(price.trim()).toFixed(2) * bookNumber;
+            totalPrice += parseFloat(price.trim()).toFixed(2) * bookNumber * 1.03;
         });
         $("tbody tr:last td:last").html("<b class='text-danger h3'>" + totalPrice.toFixed(2) + " $</b>");
     });
@@ -137,6 +190,15 @@ $(document).ready(function () {
                 $("#bodyCart").empty();
                 $("#bodyCart").load("../html/payment.html");
             }
+        }
+    });
+
+    // process check one -> all
+    $("input[name='select']").click(function () {
+        if ($("input[name='select']:checked").length == $("input[name='select']").length) {
+            $("#checkAllCart").prop('checked', true);
+        } else {
+            $("#checkAllCart").prop('checked', false);
         }
     });
 });
